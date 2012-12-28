dc6079b5fcbf57c763670fafd0aaed66b061053e
pauly <pauly@clarkeology.com>
2012-12-28 16:01:46 +0000

    refactored with dimming, comments, and tests

--- lib/lightwaverf.rb
+++ lib/lightwaverf.rb
@@ -1,13 +1,63 @@
 class LightWaveRF
-  def config
+
+  @config_file = nil
+
+  def set_config_file file
+    @config_file = file
+  end
+
+  def get_config_file
+    @config_file || File.expand_path('~') + '/lightwaverf-config.yml'
+  end
+
+  def get_config
     require 'yaml'
-    file = File.expand_path('~') + '/lightwaverf-config.yml'
-    if ! File.exists? file
-      File.open( file, 'w' ) do | handle |
+    if ! File.exists? self.get_config_file
+      File.open( @config_file, 'w' ) do | handle |
         handle.write YAML.dump( { 'host' => '192.168.0.14', 'room' => { 'our' => [ 'light', 'lights' ] } } )
       end
     end
-    YAML.load_file file
+    YAML.load_file self.get_config_file
+  end
+
+  def self.get_rooms config = { 'room' => { }}
+    rooms = { }
+    r = 1
+    config['room'].each do | name, devices |
+      rooms[name] = { 'id' => 'R' + r.to_s, 'device' => { }}
+      d = 1
+      devices.each do | device |
+        rooms[name]['device'][device] = 'D' + d.to_s
+        d += 1
+      end
+      r += 1
+    end
+    rooms
+  end
+
+  def self.get_state state = 'on'
+    case state
+      when 'off'
+        state = 'F0'
+      when 'on'
+        state = 'F1'
+      when 1..99
+        state = 'FdP' + ( state * 0.32 ).round.to_s
+    end
+    state
+  end
+
+  # Get the command to send to the wifi link
+  #
+  # Example:
+  #   >> LightWaveRF.new.command 'our', 'light', 'on'
+  #
+  # Arguments:
+  #   room: (String)
+  #   device: (String)
+  #   state: (String)
+  def command room, device, state
+    "666,!" + room['id'] + room['device'][device] + state + "|"
   end
 
   # Turn one of your devices on or off
@@ -19,36 +69,16 @@ class LightWaveRF
   #   room: (String)
   #   device: (String)
   #   state: (String)
-  def go room, device, state
+  def go room, device, state = 'on', debug = false
     require 'socket'
-    config = self.config
-    rooms = { }
-    r = 1
-    config['room'].each do | name, devices |
-      rooms[name] = {
-        'id' => 'R' + r.to_s,
-        'device' => { }
-      }
-      d = 1
-      devices.each do | device |
-        rooms[name]['device'][device] = 'D' + d.to_s
-        d += 1
-      end
-      r += 1
-    end
+    config = self.get_config
+    debug && ( p 'config is ' + config.to_s )
+    rooms = self.class.get_rooms config
     room = rooms[room]
-    state ||= 'on'
+    state = self.class.get_state state
     room && device && state && room['device'][device] || abort( "usage: #{__FILE__} [" + rooms.keys.join( "|" ) + "] light on" )
-    case state
-      when 'off'
-        state = 'F0'
-      when 'on'
-        state = 'F1'
-      when 'setup'
-        state = 'F1'
-      when 1..99
-        # @todo dimming etc
-    end
-    UDPSocket.new.send "666,!" + room['id'] + room['device'][device] + state + "|", 0, config['host'], 9760
+    command = self.command room, device, state
+    debug && ( p 'command is ' + command )
+    UDPSocket.new.send command, 0, config['host'], 9760
   end
 end
--- lightwaverf.gemspec
+++ lightwaverf.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name        = 'lightwaverf'
-  s.version     = '0.0.1'
+  s.version     = '0.0.3'
   s.date        = '2012-12-28'
   s.summary     = 'Home automation'
   s.description = 'Interact with lightwaverf wifi link from the command line'
