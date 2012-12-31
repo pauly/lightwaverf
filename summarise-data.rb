86e8d6afc98f776ce86ca3ab3e9d2c5004611653
pauly <pauly@clarkeology.com>
2012-12-31 22:52:21 +0000

    changed data format for annotated chart. not important for the gem

--- summarise-data.rb
+++ summarise-data.rb
@@ -6,8 +6,9 @@ require 'json'
 data = []
 File.open( "/home/pi/lightwaverf.log", "r" ).each_line do |line|
   line = JSON.parse( line )
-  data << [ line['timestamp'][11..15] + ' ' + line['timestamp'][5..9], line['message']['usage'] ]
+  # data << [ line['timestamp'][11..15] + ' ' + line['timestamp'][5..9], line['message']['usage'] ]
+  data << [ 'new Date("' + line['timestamp'][0..15] + '")', line['message']['usage'], line['message']['annotation'] ? line['message']['annotation']['title'] : '', line['message']['annotation'] ? line['message']['annotation']['text'] : '' ]
 end
 File.open( "/home/pi/lightwaverf-summary.json", "w" ) do |file|
-  file.write data.last( 1440 ).unshift( [ 'timestamp', 'usage' ] ).to_json
+  file.write data.last( 1440 ).to_json.gsub( /\\/, '' ).gsub( /"new/, 'new' ).gsub( /\)",/, '),' )
 end
