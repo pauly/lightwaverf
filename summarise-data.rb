84a42c31e99c1b34950d6c1e311240f1281dd983
pauly <pauly@clarkeology.com>
2013-01-02 22:30:37 +0000

    compress the data a bit for page loading

--- summarise-data.rb
+++ summarise-data.rb
@@ -6,9 +6,17 @@ require 'json'
 data = []
 File.open( "/home/pi/lightwaverf.log", "r" ).each_line do |line|
   line = JSON.parse( line )
-  # data << [ line['timestamp'][11..15] + ' ' + line['timestamp'][5..9], line['message']['usage'] ]
-  data << [ 'new Date("' + line['timestamp'][0..15] + '")', line['message']['usage'], line['message']['annotation'] ? line['message']['annotation']['title'] : '', line['message']['annotation'] ? line['message']['annotation']['text'] : '' ]
+  # data << [ 'new Date("' + line['timestamp'][0..15] + '")', line['message']['usage'], line['message']['annotation'] ? line['message']['annotation']['title'] : '', line['message']['annotation'] ? line['message']['annotation']['text'] : '' ]
+  new_line = []
+  # new_line << line['timestamp'][0..15] # original date
+  new_line << line['timestamp'][2..3] + line['timestamp'][5..6] + line['timestamp'][8..9] + line['timestamp'][11..12] + line['timestamp'][14..15] # compact version of date
+  new_line << line['message']['usage'] / 10
+  if line['message']['annotation']
+    new_line << line['message']['title']
+    new_line << line['message']['text']
+  end
+  data << new_line
 end
 File.open( "/home/pi/lightwaverf-summary.json", "w" ) do |file|
-  file.write data.last( 1440 ).to_json.gsub( /\\/, '' ).gsub( /"new/, 'new' ).gsub( /\)",/, '),' )
+  file.write data.last( 1440 * 7 ).to_json.gsub( /"/, '' )
 end
