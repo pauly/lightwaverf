03cf61eac1ac873a984a27107bb88b775d1e0b01
pauly <pauly@clarkeology.com>
2013-02-08 14:51:50 +0000

    doc changes

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 sudo apt-get update
 sudo apt-get upgrade
-yes | sudo apt-get install git-core ruby
-git clone git@github.com:pauly/lightwaverf.git
+# sudo apt-get install git-core ruby
+git clone git://github.com/pauly/lightwaverf.git # or git clone git@github.com:pauly/lightwaverf.git
 cd lightwaverf/
 crontab cron.tab
-gem build lightwaverf.gemspec 
-sudo gem install ./lightwaverf-0.2.1.gem # or whatever the latest version is
-vi ~/lightwaverf-config.yml 
-lightwaverf dining lights on
+sudo gem install lightwaverf
+# gem build lightwaverf.gemspec && sudo gem install ./lightwaverf-0.2.1.gem # or whatever the latest version is
+cp lightwaverf-config.yml ~ && vi ~/lightwaverf-config.yml 
+lightwaverf dining lights on # and pair this device on your wifi link
